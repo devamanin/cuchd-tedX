81c9d841b512083700326dea59500bf1f3f7fd07
Vansh <vansh8755381346@gmail.com>
2022-09-30 19:41:15 +0530

    new

--- src/Homecomp/Head.jsx
+++ src/Homecomp/Head.jsx
@@ -24,8 +24,9 @@ function CollapsibleExample() {
             <Nav.Link eventKey={2} href="#blog" style={{ color: "white" }}>Blogs</Nav.Link>
             <Nav.Link href="#about" style={{ color: "white" }}>About</Nav.Link>
             <Nav.Link href="#cuchd" style={{ color: "white" }}>Cuchd</Nav.Link>
-            <Nav.Link href="#teams" style={{ color: "white" }}>Teams</Nav.Link>
             <Nav.Link eventKey={2} href="#theme" style={{ color: "white" }}>Theme</Nav.Link>
+            <Nav.Link href="#teams" style={{ color: "white" }}>Teams</Nav.Link>
+            {/* <Nav.Link eventKey={2} href="#theme" style={{ color: "white" }}>Theme</Nav.Link> */}
             {/* <Nav.Link eventKey={2} href="#memes" style={{ color: "white" }}>Help</Nav.Link> */}
           </Nav>
         </Navbar.Collapse>
